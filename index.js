c59c2e1c0183fae8ee8085c99388a999fbc9fc09
Victor Solique <victordesouzasolique@gmail.com>
2024-11-12 21:24:24 -0400

    commit 06/11
    Tinha um commit feito para o dia 6/11, porém acabei esquecendo

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const prisma = require("./prisma/prismaClient");
 
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json())
 
 const authRouters = require("./routes/authRoutes");
 app.use("/auth", authRouters);
 
 app.listen(8000);
+
+
